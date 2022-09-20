ef0858fe4bb0b60a564c1bc1fa1a5a8919daf365
bsirikam <buntakan0703@gmail.com>
2022-09-20 09:18:09 +0700

    add header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bsirikam <bsirikam@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 19:38:58 by bsirikam          #+#    #+#             */
-/*   Updated: 2022/09/19 23:54:53 by bsirikam         ###   ########.fr       */
+/*   Updated: 2022/09/20 09:18:03 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ int	ft_putchar(char c);
 int	ft_strlen(char *s);
 int	ft_putstr(char *s);
 int ft_putnbr(int n);
+int	ft_unit(unsigned int n);
 
 #endif
